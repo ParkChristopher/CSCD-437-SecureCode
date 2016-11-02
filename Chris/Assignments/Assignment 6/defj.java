ec9cfededd14806d52c64f0db5e99ad70a6fce42
Chris Park <christopherpark@eagles.ewu.edu>
2016-11-02 08:55:38 -0700

    pulled changes from homero's build

--- Chris/Assignments/Assignment 6/defj.java
+++ Chris/Assignments/Assignment 6/defj.java
@@ -4,8 +4,8 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.regex.*;
-import java.math.BigInteger;
 import java.lang.Exception;
+import java.security.SecureRandom;
 
 public class defj{
 
@@ -14,18 +14,18 @@ public class defj{
 		Scanner scan;
 		PrintWriter writer;
 		String firstName, lastName, inFileName, outFileName;
-		long val1, val2;
+		int val_array[] = new int[2];
 		
 		scan = new Scanner(System.in);
 		
 		firstName = getString("first name", scan);
 		lastName = getString("last name", scan);
-		val1 = getInt(scan);
-		val2 = getInt(scan);
+		getInt(val_array, scan);
 		inFileName = getFileName("input", scan);
 		outFileName = getFileName("output", scan);
-		//Get Password
-		//Verify Password
+		getPassword(scan);
+		verifyPassword(scan);
+		
 		//Write to ouput
 		scan.close();
 	}
@@ -58,36 +58,80 @@ public class defj{
 	/**
 	* Integers
 	*/
-	public static long getInt(Scanner scan){
+	public static void getInt(int[] nums, Scanner scan){
 		
-		BigInteger bigInt;
 		long val = 0;
 		boolean done = false;
+		int temp;
 		
-		
-		while(!done){
+		do{
+			System.out.print("Enter your first 32bit number: ");
 			
-			System.out.print("Please enter a value: ");
-			
-			try{
+			while(!scan.hasNextInt()){
 				
-					bigInt = BigInteger.valueOf(scan.nextInt());
-					
-					if(bigInt.bitLength() > 31) throw new IllegalArgumentException();
-					
-					val = bigInt.longValue();
-					done = true;
+				System.out.print("Enter your first 32bit number: ");
+				scan.next();
 			}
-			catch(Exception e){
-				
-				System.out.print("Invalid input:");
-				System.out.println("Expected 32 bit integer.");
+			
+		nums[0] = scan.nextInt();
+		
+		System.out.print("Enter your second 32bit number: ");
+		
+		while(!scan.hasNextInt()){
+			
+				System.out.print("Enter your first 32bit number: ");
+				scan.next();
+			}
+			
+			nums[1] = scan.nextInt();
+		
+		}while(!check(nums));
+	}
+
+	/**
+	* helper function for number verification and overflow
+	*/
+	private static boolean check(int[] numbers)
+	{
+		if(numbers[0]>=0 && numbers[1]>=0)
+		{
+			if(numbers[0] + numbers[1] < 0)
+			{
+				System.out.println("Number combinations are invalid");
+				return false;
+			}
+			else if(numbers[0] * numbers[1] < 0)
+			{
+				System.out.println("Number combinations are invalid");
+				return false;
 			}
 		}
 		
-		return val;
+		else if(numbers[0]<=0 && numbers[1]<=0)
+		{
+			if(numbers[0] + numbers[1] > 0)
+			{
+				System.out.println("Number combinations are invalid");
+				return false;
+			}
+			else if(numbers[0] * numbers[1] < 0)
+			{
+				System.out.println("Number combinations are invalid");
+				return false;
+			}
+		}
+		
+		else if(numbers[0]<=0 && numbers[1]>=0 || numbers[0]>=0 && numbers[1]<=0)
+		{
+			 if(numbers[0] * numbers[1] >= 0)
+			{
+				System.out.println("Number combinations are invalid");
+				return false;
+			}
+		}
+		return true;
 	}
-
+	
 	/**
 	* File Name
 	*/
@@ -114,28 +158,102 @@ public class defj{
 		return input;
 	}
 
-
-//get a password from the user
-//minimum x characters
-//upper case, lower case, punctuation, numeric
-//salt and hash
-//get verification after entering
-	
-	//Password
-	public static void getPassword(Scanner scan){
-		// While not done
-			// Get password
-				//	If password passes regex
-					//salt and hash it
-					//ask for verification (Re-entry of password)
-			//if verification passed
-				//store (how?)
-	}
+//Password
+   public static void getPassword(Scanner scan){
+   	// While not done
+   		// Get password
+   			//	If password passes regex
+   				//salt and hash it
+   				//ask for verification (Re-entry of password)
+   		//if verification passed
+   			//store (how?)
+           
+      String input = "";
+      boolean done = false; 
+      Pattern pattern = Pattern.compile("^[A-Za-z0-9!?]{1,}$");
+      scan.nextLine();
+      while(!done){
+      	
+         System.out.print("Please enter your password");
+         input = scan.nextLine();
+      	
+         if(isMatched(input, pattern)) done = true;
+         else {
+         	
+            System.out.println("Invalid input:");
+            System.out.println("Your password need to contain a upper case, lower case, punctuation,and numeric value");
+         	
+         }
+      }
+      byte [] salt = new byte[input.length()];
+      SecureRandom ran = new SecureRandom();
+      ran.nextBytes(salt);
+      input = salt + input;     
+           
+      byte [] hashedInput = new byte[input.length()+224];
+   	
+      Whirlpool w = new Whirlpool();
+      w.NESSIEinit();
+      w.NESSIEadd(input);
+      w.NESSIEfinalize(hashedInput); 
+      String hash = w.display(hashedInput);
+      try{
+         PrintWriter writer = new PrintWriter(new File("test.txt"));
+         writer.println(salt);
+         writer.println(hash);
+         writer.close();
+         
+       }catch(IOException e) {}
+   }
 	
 	//Verify Password
-	public static void verifyPassword(Scanner scan){
-		
-	}
+   public static void verifyPassword(Scanner scan){
+   String salt = "";
+   String password = "";
+   
+   try{
+      Scanner file = new Scanner(new File("test.txt"));
+      salt = file.nextLine();
+      password = file.nextLine();
+      file.close();
+   }catch(IOException e) {}
+   
+      String input = "";
+      boolean done = false; 
+      Pattern pattern = Pattern.compile("^[A-Za-z0-9!?]{1,}$");
+      
+      while(!done){
+      	
+         System.out.print("Please re-enter your password for confirmation");
+         input = scan.nextLine();
+      	
+         if(isMatched(input, pattern)) done = true;
+         else {
+         	
+            System.out.println("Invalid input:");
+            System.out.println("Your password need to contain a upper case, lower case, punctuation,and numeric value");
+         	
+         }
+      }
+      
+      input = salt + input;     
+           
+      byte [] hashedInput = new byte[input.length()+224];
+     
+      Whirlpool w = new Whirlpool();
+      w.NESSIEinit();
+      w.NESSIEadd(input);
+      w.NESSIEfinalize(hashedInput); 
+      String hash = w.display(hashedInput);
+      if(hash.compareTo(password)== 0)
+      {
+         System.out.println("PassWord has been Confirmed");
+      }
+      else
+      {
+         System.out.println("Passwords did not match");
+      }
+   }
 	
 
 //open output file and write:
