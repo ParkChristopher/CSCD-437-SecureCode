acf601a55111802b7f8817c07388c339531740dc
ParkChristopher <christopherpark@eagles.ewu.edu>
2016-11-04 01:27:43 -0700

    Testing and formatting for Java code.

--- Assignment 6 Trunk/Java/defj.java
+++ Assignment 6 Trunk/Java/defj.java
@@ -73,36 +73,35 @@ public class defj{
 		int [] numbers = new int[2];
 		int temp;
 		
-		do
-		{ 
+		do{
+       
 			System.out.println("Enter Your first 32bit Number");
-			while(!kb.hasNextInt())
-			{
+			while(!kb.hasNextInt()){
+
 				System.out.println("Enter Your first 32bit Number");
 				kb.next();
 			}
 			
 			temp = kb.nextInt();
-			
 			numbers[0] = temp;
 			
 			System.out.println("Enter Your second 32bit Number");
-			while(!kb.hasNextInt())
-			{
+			while(!kb.hasNextInt()){
+
 				System.out.println("Enter Your second 32bit Number");
 				kb.next();
 			}
 			
 			temp = kb.nextInt();
 			numbers[1] = temp;
-			
-		
 		}while(!check(numbers));
-		
 		
 		return numbers;
 	}
 
+   /**
+   * Verify Arithmetic Operations
+   */
 	private static boolean check(int[] numbers)
 	{
 		if(numbers[0]>=0 && numbers[1]>=0)
@@ -146,52 +145,47 @@ public class defj{
 
 
 	/**
-	* File Name
+	* Get A File Name
 	*/
    public static String getFileName(String type,String inputFileName, Scanner scan){
    	
       String input = "";
       boolean done = false; 
-      Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\-_]+.{1}txt$");
+      Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+.{1}txt$");
    	
       while(!done){
       	
          System.out.print("Please enter  an " + type + " file name: ");
          input = scan.next();
       	
-         if(isMatched(input, pattern))
-         {
-            if( input.compareTo("test.txt") == 0)
-            {
-               System.out.print("Invalid input:");
-               System.out.println("File Name selected is not allowed");
+         if(isMatched(input, pattern)){
+
+            if( input.compareTo("test.txt") == 0){
+               System.out.print("Invalid input. ");
+               System.out.println("File name selected is not allowed.");
             }
-            else if(type.compareTo("output")==0)
-            {
-               if(inputFileName.compareTo(input)==0)
-               {
-                System.out.print("Invalid input:");
-                System.out.println("File Name was selected as input file name");
-               }
-               else
-                  done = true;
-            }
-            else 
-               done = true;
-         }
-          
-         else {
+            else if(type.compareTo("output")==0){
+
+               if(inputFileName.compareTo(input)==0) {
+
+                System.out.print("Invalid input. ");
+                System.out.println("File Name was selected as input file name.");
+               } else done = true;
+            } else done = true;
+         } else {
          	
             System.out.print("Invalid input:");
             System.out.print("Expected valid .txt file name made up of characters,");
-            System.out.println("numbers, and (-_). Example: <my-file_name9.txt>");
+            System.out.println("and numbers. Example: <myfilename9.txt>");
          }
       }
-   	
+
       return input;
    }
 	
-	//Password
+	/**
+   * Get A Password
+   */
    public static void getPassword(Scanner scan){
           
       String input = "";
@@ -208,9 +202,9 @@ public class defj{
          	
             System.out.println("Invalid input:");
             System.out.println("Your password need to contain a upper case, lower case, punctuation,and numeric value");
-         	
          }
       }
+
       byte [] salt = new byte[input.length()];
       SecureRandom ran = new SecureRandom();
       ran.nextBytes(salt);
@@ -230,13 +224,13 @@ public class defj{
          writer.println(hash);
          writer.close();
          
-       }catch(IOException e)
-       {
-       }
-         
+       }catch(IOException e) 
+       {} 
    }
 	
-	//Verify Password
+	/**
+   * Verify The Password
+   */
    public static boolean verifyPassword(Scanner scan){
 
    String salt = "";
@@ -247,7 +241,7 @@ public class defj{
       salt = file.nextLine();
       password = file.nextLine();
       file.close();
-   }catch(IOException e)
+   }catch(IOException e) 
    {}
    
       String input = "";
@@ -269,35 +263,27 @@ public class defj{
       }
       
       input = salt + input;     
-           
       byte [] hashedInput = new byte[input.length()+224];
-     
    	
       Whirlpool w = new Whirlpool();
       w.NESSIEinit();
       w.NESSIEadd(input);
       w.NESSIEfinalize(hashedInput); 
       String hash = w.display(hashedInput);
-      if(hash.compareTo(password)== 0)
-      {
+      if(hash.compareTo(password)== 0){
+
          System.out.println("Password confirmed.");
          return true;
-      }
-      else
-      {
+      } else {
+         
          System.out.println("Passwords did not match");
          return false;
       }
    }
 	
-
-//open output file and write:
-//user name
-//result of addition and multiplying the integer values
-//contents of input file
-
-   
-	
+   /**
+   * Write Output to File
+   */
    public static void writeToOutput(Scanner scan, PrintWriter outFile,int []numbers, String firstName,String lastName)
    {
       String transfer = "";
@@ -305,48 +291,51 @@ public class defj{
    	outFile.println("Sum = " + (numbers[0] + numbers[1]));
       outFile.println("Product = " +numbers[0] * numbers[1]);
       
-      if(scan != null)
-      {
-         while(scan.hasNextLine())
-         {
+      if(scan != null) {
+         while(scan.hasNextLine()){
+
             transfer = scan.nextLine();
             outFile.println(transfer);
-            
          }
+
          scan.close();
-         
       }
-   outFile.close();
+
+      outFile.close();
    }
    
    
-//Hook Up scanner to read file
+   /**
+   * Prepare For File Input.
+   */
    public static Scanner inputFileHookUp(Scanner scan,  String input){
    
       try{
       
        scan = new Scanner(new File(input));
-       
-      
-      }catch(IOException e)
-      {
+      }catch(IOException e){
+
          return null;
       }
       return scan;
    }
    
-//Hook up PrintWriter to write to file   
+    /**
+    * Prepare For File Output.
+    */  
     public static PrintWriter outputFileHookUp(PrintWriter writer,  String output){
    
       try{
       
        writer = new PrintWriter(new File(output));
-
       }catch(IOException e){}
       
       return writer;
    }
 
+   /**
+   * Attempt To Match Pattern To String
+   */
    public static boolean isMatched(String str, Pattern pattern){
    
       Matcher matcher = pattern.matcher(str);
